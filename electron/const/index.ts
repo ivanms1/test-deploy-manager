d758c6907a316f23625f3e58d6dc4c891e72b6c4
Ivan Saldano <ivanms1@gmail.com>
2021-04-28 13:54:33 +0900

    second commit

---
+++ electron/const/index.ts
@@ -0,0 +1,9 @@
+export const API_URI = "http://192.168.100.54:4000/api/v1";
+
+export const ORG_NAME = "Org1";
+
+export const FcnTypes = {
+  CreateFile: "CreateFile",
+  LikeContent: "LikeContent",
+  CountDownloads: "CountDownloads",
+};
---
+++ src/const/index.ts
@@ -0,0 +1,52 @@
+import useGetConBalance from "../hooks/useGetConBalance";
+import useGetConTokenBalance from "../hooks/useGetConTokenBalance";
+import useGetEthBalance from "../hooks/useGetEthBalance";
+
+export const serverUrl = "http://192.168.100.54:4000/api/v1/";
+
+export const AUTH_TOKEN = "conun-auth-token";
+
+export const PUBLIC_KEY_TOKEN = "conun-public-key";
+
+export const PRIVATE_KEY_TOKEN = "conun-private-key";
+
+export const WALLET_PRIVATE_KEY_TOKEN = "conun-wallet-private-key";
+
+export const WALLET_ADDRESS_TOKEN = "conun-wallet-address";
+
+export const API_URL_TOKEN = "conun-api-url";
+
+export const KEY_STORE_TOKEN = "conun-key-store";
+
+export const CONUN_PASS_TOKEN = "conun-pass";
+
+export const ORG_NAME = "Org1";
+
+export const WALLET_TYPE = "ETH";
+
+export const IDENTITY = "identity";
+
+export const FcnTypes = {
+  Transfer: "Transfer",
+  Init: "Init",
+  Mint: "Mint",
+  Burn: "Burn",
+  GetDetails: "GetDetails",
+  BalanceOf: "BalanceOf",
+};
+
+export const TOKEN_CARDS = [
+  {
+    token: "conx",
+    useBalance: useGetConTokenBalance,
+    useGasEstimate: () => ({ data: null, loading: false }),
+  },
+  {
+    token: "eth",
+    useBalance: useGetEthBalance,
+  },
+  {
+    token: "con",
+    useBalance: useGetConBalance,
+  },
+];

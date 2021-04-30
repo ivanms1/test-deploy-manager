f3f6657c836dc6ea768e196fdc00572de064e25f
Ivan Saldano <ivanms1@gmail.com>
2021-04-30 15:27:46 +0900

    spinner main

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,7 +4,10 @@ import classNames from "classnames";
 import Footer from "./Footer";
 import SettingsSidebar from "./SettingsSidebar";
 import RecentTransactions from "./RecentTransactions";
+import Spinner from "../Spinner";
 
+import useCurrentUser from "../../hooks/useCurrentUser";
+import useAppCurrentUser from "../../hooks/useAppCurrentUser";
 import { useAppContext } from "../AppContext";
 
 import styles from "./Layout.module.scss";
@@ -16,18 +19,29 @@ interface LayoutProps {
 function Layout({ children }: LayoutProps) {
   const { isAuthenticated } = useAppContext();
 
+  const { isLoading } = useCurrentUser();
+  const { isLoading: appUserLoading } = useAppCurrentUser();
+
   return (
     <>
-      {isAuthenticated && <SettingsSidebar />}
-      <div
-        className={classNames(styles.Layout, {
-          [styles.isAuthenticated]: isAuthenticated,
-        })}
-      >
-        {children}
-      </div>
-      <Footer />
-      {isAuthenticated && <RecentTransactions />}
+      {isLoading || appUserLoading ? (
+        <div className={styles.SpinnerContainer}>
+          <Spinner />
+        </div>
+      ) : (
+        <>
+          {isAuthenticated && <SettingsSidebar />}
+          <div
+            className={classNames(styles.Layout, {
+              [styles.isAuthenticated]: isAuthenticated,
+            })}
+          >
+            {children}
+          </div>
+          <Footer />
+          {isAuthenticated && <RecentTransactions />}
+        </>
+      )}
     </>
   );
 }
---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./Spinner.module.scss";
+
+function Spinner() {
+  return <div className={styles.Spinner} />;
+}
+
+export default Spinner;
